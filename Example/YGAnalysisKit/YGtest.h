13d7fa1774f533de9a136617379b4830d97bfd89
wyg <gjzxwyg@163.com>
2020-09-19 20:41:39 +0800

    test

---
+++ Example/YGAnalysisKit/YGtest.h
@@ -0,0 +1,17 @@
+//
+//  YGtest.h
+//  YGAnalysisKit_Example
+//
+//  Created by wyg on 2020/9/19.
+//  Copyright © 2020 gjzxwyg@163.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YGtest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/YGAnalysisKit/YGtest.m
@@ -0,0 +1,13 @@
+//
+//  YGtest.m
+//  YGAnalysisKit_Example
+//
+//  Created by wyg on 2020/9/19.
+//  Copyright © 2020 gjzxwyg@163.com. All rights reserved.
+//
+
+#import "YGtest.h"
+
+@implementation YGtest
+
+@end
